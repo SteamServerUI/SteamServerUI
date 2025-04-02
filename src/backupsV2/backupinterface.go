009690c2029abf24dccbc3bc1cc7cfafcaa1e80b
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2025-04-02 17:09:20 +0200

    refactor backup config to use user or default configured vars from global config

--- src/backupsV2/backupinterface.go
+++ src/backupsV2/backupinterface.go
@@ -3,7 +3,6 @@ package backupsv2
 
 import (
 	"StationeersServerUI/src/config"
-	"path/filepath"
 	"time"
 )
 
@@ -21,20 +20,18 @@ func InitGlobalBackupManager(config BackupConfig) error {
 
 // GetBackupConfig returns a properly configured BackupConfig
 func GetBackupConfig() BackupConfig {
-	backupDir := filepath.Join("./saves/" + config.WorldName + "/Backup")
-	safeBackupDir := filepath.Join("./saves/" + config.WorldName + "/Safebackups")
 
 	return BackupConfig{
 		WorldName:     config.WorldName,
-		BackupDir:     backupDir,
-		SafeBackupDir: safeBackupDir,
+		BackupDir:     config.ConfiguredBackupDir,
+		SafeBackupDir: config.ConfiguredSafeBackupDir,
 		WaitTime:      30 * time.Second,
 		RetentionPolicy: RetentionPolicy{
-			KeepLastN:       10,
-			KeepDailyFor:    30 * 24 * time.Hour,
-			KeepWeeklyFor:   90 * 24 * time.Hour,
-			KeepMonthlyFor:  365 * 24 * time.Hour,
-			CleanupInterval: 1 * time.Hour,
+			KeepLastN:       config.BackupKeepLastN,
+			KeepDailyFor:    config.BackupKeepDailyFor,
+			KeepWeeklyFor:   config.BackupKeepWeeklyFor,
+			KeepMonthlyFor:  config.BackupKeepMonthlyFor,
+			CleanupInterval: config.BackupKeepMonthlyFor,
 		},
 	}
 }
