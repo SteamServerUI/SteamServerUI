86bbe154548692e450121d07a3c0864d19d667a5
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2025-04-03 03:21:10 +0200

    first pass on update check and executable updater for Linux and Windows platforms

---
+++ src/install/update-helper-linux.go
@@ -0,0 +1,14 @@
+//go:build linux
+
+package install
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func setSysProcAttr(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true, // Unix detachment via new process group
+	}
+}
