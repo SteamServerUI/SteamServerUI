2d40a7cf77e417180ffa673f7b3b8a16ccf5ff8e
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2025-04-05 13:57:19 +0200

    changed discord status text to show version and StationeersServerUI

--- src/config/config.go
+++ src/config/config.go
@@ -66,7 +66,7 @@ type CustomDetection struct {
 }
 
 var (
-	Version = "4.5.16"
+	Version = "4.5.18"
 	Branch                  = "nightly-v4-5-0"
 	GameBranch              string
 	DiscordToken            string
