e92d75fb462c7f45f6c108dff37ed6013ba2947a
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2025-04-05 15:27:51 +0200

    fix: call gamemgr diretcy for status

--- src/config/config.go
+++ src/config/config.go
@@ -66,7 +66,7 @@ type CustomDetection struct {
 }
 
 var (
-	Version = "4.5.32"
+	Version = "4.5.33"
 	Branch                  = "nightly-v4-5-0"
 	GameBranch              string
 	DiscordToken            string
