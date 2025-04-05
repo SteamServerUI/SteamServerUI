8eff831c09ae00777957c1822ee63ff6f542e19a
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2025-04-05 14:59:57 +0200

    improve slash command registration by checking for existing commands and updating only if necessary

--- src/config/config.go
+++ src/config/config.go
@@ -66,7 +66,7 @@ type CustomDetection struct {
 }
 
 var (
-	Version = "4.5.24"
+	Version = "4.5.28"
 	Branch                  = "nightly-v4-5-0"
 	GameBranch              string
 	DiscordToken            string
