2da58e1fb46b10403fcb0bfbcbd3ef82baca256c
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2025-04-05 16:17:18 +0200

    Server status now outputs pretty values

--- src/config/config.go
+++ src/config/config.go
@@ -66,7 +66,7 @@ type CustomDetection struct {
 }
 
 var (
-	Version = "4.5.33"
+	Version = "4.5.37"
 	Branch                  = "nightly-v4-5-0"
 	GameBranch              string
 	DiscordToken            string
