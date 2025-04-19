ce486b0fcf15269602f2184e8a323496a74124e1
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2025-04-20 00:57:52 +0200

    add HandleRunSteamCMD and /api/v2/steamcmd/run for later use

--- src/web/start.go
+++ src/web/start.go
@@ -75,11 +75,15 @@ func StartWebServer(wg *sync.WaitGroup) {
 	protectedMux.HandleFunc("/api/v2/args/update", updateRunfileHandler) // List all arguments
 	protectedMux.HandleFunc("/api/v2/args/save", saveRunfileHandler)
 
+	// runfile routes
 	protectedMux.HandleFunc("/api/v2/runfile/groups", HandleRunfileGroups)
 	protectedMux.HandleFunc("/api/v2/runfile/args", HandleRunfileArgs)
 	protectedMux.HandleFunc("/api/v2/runfile/args/update", HandleRunfileArgUpdate)
 	protectedMux.HandleFunc("/api/v2/runfile", HandleRunfile)
 	protectedMux.HandleFunc("/api/v2/runfile/save", HandleRunfileSave)
+
+	// steamcmd routes
+	protectedMux.HandleFunc("/api/v2/steamcmd/run", HandleRunSteamCMD)
 
 	// Apply middleware only to protected routes
 	mux.Handle("/", AuthMiddleware(protectedMux)) // Wrap protected routes under root
