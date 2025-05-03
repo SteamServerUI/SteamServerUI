7ded1a48b8ea8b0e193538f45d4ea8f6775c9e5d
JacksonTheMaster <jakob.langisch@gmail.com>
2025-05-03 22:08:06 +0000

    Add initial Electron configuration and integrate main script in package.json for app launching

---
+++ ssui-interfacev2/main.cjs
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadURL('http://localhost:5173'); // Vite’s dev server
+}
+
+app.commandLine.appendSwitch('ignore-certificate-errors');
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
