e48d6b482048ae57f8821bb0186cd39f9354f869
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2025-05-04 23:16:23 +0000

    update server setup to try array default ports

--- ssui-interfacev2/main.cjs
+++ ssui-interfacev2/main.cjs
@@ -63,8 +63,8 @@ async function createWindow() {
   if (!serverStarted) return;
 
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true
