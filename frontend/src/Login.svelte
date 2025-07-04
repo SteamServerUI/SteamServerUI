81b7d60882690920e61047c1f9fdde349d4b8609
JacksonTheMaster <jakob.langisch@gmail.com>
2025-07-04 23:33:08 +0000

    fixed a race condition between login POST and browser default page reload on submit. this affected and fixes the auth issue in the Electron app. The issue was reproducable in the browser too, but more unlikely to happen.

--- frontend/src/Login.svelte
+++ frontend/src/Login.svelte
@@ -386,7 +386,7 @@ async function checkBackendStatus(id) {
           </div>
         </div>
       {:else}
-        <form onsubmit={(handleSubmit)}>
+        <form onsubmit={preventDefault(handleSubmit)}>
           <div class="form-group">
             <label for="username">Username</label>
             <div class="input-wrapper">
