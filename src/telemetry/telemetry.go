a3c0c42c5c7e853f9a53fcc367249eaeb7004ec0
JacksonTheMaster <jakob.langisch@gmail.com>
2025-07-18 19:03:23 +0200

    update telemetry module to send data to local server

--- src/telemetry/telemetry.go
+++ src/telemetry/telemetry.go
@@ -1,7 +1,10 @@
 package telemetry
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"net/http"
 	"runtime"
 
 	"github.com/SteamServerUI/SteamServerUI/v6/src/config"
@@ -23,14 +26,15 @@ func InitTelemetry() {
 		return
 	}
 
-	masterServerFQDN := "https://io.jxsn.dev/api/v1/telemetry" // does not exist yet
+	//masterServerFQDN := "https://io.jxsn.dev/api/v1/telemetry" // does not exist yet
+	masterServerFQDN := "http://localhost:8080/api/v1/telemetry" // for testing
 	harvestData := harvestTelemetry()
 	logger.Security.Infof("Initializing telemetry module...")
 	logger.Security.Debugf("Supports Sprintf formatting")
 	logger.Security.Warnf("This is how to use the logger")
 	logger.Security.Errorf("some sting with %s", masterServerFQDN)
 
-	sendTelemetry(harvestData)
+	sendTelemetry(harvestData, masterServerFQDN)
 }
 
 func harvestTelemetry() *TelemetryData {
@@ -44,11 +48,21 @@ func harvestTelemetry() *TelemetryData {
 	return &telemetryData
 }
 
-func sendTelemetry(harvestData *TelemetryData) {
-	// TODO: Send telemetry data to actual master server. for now, we just log it.
+func sendTelemetry(harvestData *TelemetryData, endpointURI string) {
 	logger.Security.Warnf("Sending telemetry data to master server...")
 	jsonData, _ := json.Marshal(harvestData)
-	logger.Security.Infof("Telemetry data: %s", string(jsonData))
+	// send data to master server
+	resp, err := http.Post(endpointURI, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		logger.Security.Error("Error sending telemetry data: " + err.Error())
+		return
+	}
+	fmt.Print(resp.StatusCode)
+	if resp.StatusCode != 200 {
+		logger.Security.Error("Error sending telemetry data: non-200 status code returned.")
+		return
+	}
+	logger.Security.Info("Telemetry data sent successfully.")
 }
 
 func getOSType() string {
