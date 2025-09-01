19f9517ee7b9f45aacfa1c7b737b59fafb884ded
JacksonTheMaster <81807824+JacksonTheMaster@users.noreply.github.com>
2025-09-01 17:20:33 +0200

    wrap https server under custom webServerLogger to suppress TLS handshake errors and friends

--- src/web/start.go
+++ src/web/start.go
@@ -2,6 +2,7 @@
 package web
 
 import (
+	"log"
 	"net/http"
 	"net/http/pprof"
 	"sync"
@@ -11,6 +12,14 @@ import (
 	"github.com/JacksonTheMaster/StationeersServerUI/v5/src/logger"
 )
 
+type webServerLogger struct{}
+
+func (cl *webServerLogger) Write(p []byte) (n int, err error) {
+	// Redirect HTTP server logs (like TLS handshake errors) to logger.Web.Debug
+	logger.Web.Debug(string(p))
+	return len(p), nil
+}
+
 func StartWebServer(wg *sync.WaitGroup) {
 
 	logger.Web.Info("Starting API services...")
@@ -19,6 +28,7 @@ func StartWebServer(wg *sync.WaitGroup) {
 	// Apply middleware only to protected routes
 	mux.Handle("/", AuthMiddleware(protectedMux)) // Wrap protected routes under root
 
+	httpLogger := log.New(&webServerLogger{}, "", 0)
 	// Start HTTP server
 	wg.Add(1)
 	go func() {
@@ -26,9 +36,17 @@ func StartWebServer(wg *sync.WaitGroup) {
 		// Ensure TLS certs are ready
 		if err := security.EnsureTLSCerts(); err != nil {
 			logger.Web.Error("Error setting up TLS certificates: " + err.Error())
-			//os.Exit(1)
+			return
 		}
-		err := http.ListenAndServeTLS("0.0.0.0:"+config.GetSSUIWebPort(), config.GetTLSCertPath(), config.GetTLSKeyPath(), mux)
+
+		// Create an HTTP server with a custom logger
+		server := &http.Server{
+			Addr:     "0.0.0.0:" + config.GetSSUIWebPort(),
+			Handler:  mux,
+			ErrorLog: httpLogger,
+		}
+
+		err := server.ListenAndServeTLS(config.GetTLSCertPath(), config.GetTLSKeyPath())
 		if err != nil {
 			logger.Web.Error("Error starting HTTPS server: " + err.Error())
 		}
